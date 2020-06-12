649e083015552c902bb51e058cebe0de83c122d1
hayakzan <61164329+hayakzan@users.noreply.github.com>
2020-06-12 02:41:28 -0700

    Add files via upload

---
+++ build/CMakeFiles/RenyiEntropy.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rikyu/Desktop/supercollider-Version-3.10.2/RenyiEntropy/RenyiEntropy.cpp" "/Users/rikyu/Desktop/supercollider-Version-3.10.2/RenyiEntropy/build/CMakeFiles/RenyiEntropy.dir/RenyiEntropy.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Users/rikyu/Desktop/supercollider-Version-3.10.2/include/plugin_interface"
+  "../Users/rikyu/Desktop/supercollider-Version-3.10.2/include/common"
+  "../Users/rikyu/Desktop/supercollider-Version-3.10.2/common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/RenyiEntropy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RenyiEntropy.dir/RenyiEntropy.cpp.o"
+  "RenyiEntropy.pdb"
+  "RenyiEntropy.scx"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RenyiEntropy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
